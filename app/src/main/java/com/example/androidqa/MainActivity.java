1be0e729812a6ac5f9f94a1f5352034dde2e2aad
foty <534728972@qq.com>
2021-02-22 16:49:10 +0800

    handler--同步屏障

--- app/src/main/java/com/example/androidqa/MainActivity.java
+++ app/src/main/java/com/example/androidqa/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.androidqa;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.MessageQueue;
+import android.os.SystemClock;
+import android.util.Log;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,40 +22,105 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        new Handler().post(new Runnable() {
+        @SuppressLint("HandlerLeak") Handler handler = new Handler() {
             @Override
-            public void run() {
-                //todo
+            public void handleMessage(@NonNull Message msg) {
+                int i = -1;
+                if (msg.obj instanceof Integer) {
+                    i = (int) msg.obj;
+                }
+                Log.d("lxx", "收到事件消息：msg.obj= " + i);
             }
-        });
+        };
 
         Message msg = new Message();
-        msg.what = 1;
         msg.obj = 100;
-        new Handler().sendMessage(msg);
+        handler.sendMessage(msg);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        Message message1 = Message.obtain();
+        handler.sendMessage(message1);
 
-            }
-        },100);
+        //发送同步屏障
+        //sendSyncBarrier(handler);
 
-        new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(@NonNull Message msg) {
-                return false;
-            }
-        });
+        //发送2个异步消息
+        Message m1 = Message.obtain();
+        m1.obj = 200;
+//        m1.setAsynchronous(true);
+        handler.sendMessage(m1);
+
+        //同步消息
+        Message m3 = Message.obtain();
+        m3.obj = 300;
+        handler.sendMessage(m3);
+
+        //异步消息
+        Message m2 = Message.obtain();
+        m2.obj = 220;
+//        m1.setAsynchronous(true);
+        handler.sendMessage(m2);
+
+        //同步消息
+        Message m4 = Message.obtain();
+        m4.obj = 300;
+        handler.sendMessage(m4);
+
+
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        }, 100);
+
+//        new Handler(new Handler.Callback() {
+//            @Override
+//            public boolean handleMessage(@NonNull Message msg) {
+//                return false;
+//            }
+//        });
 
         //使用 IdleHandler
-        new Handler().getLooper().getQueue().addIdleHandler(new MessageQueue.IdleHandler() {
-            @Override
-            public boolean queueIdle() {
-                return false;
-            }
-        });
+//        handler.getLooper().getQueue().addIdleHandler(new MessageQueue.IdleHandler() {
+//            @Override
+//            public boolean queueIdle() {
+//                return false;
+//            }
+//        });
+
+        //开启同步屏障 这个方法标注为 hide。
+        // handler.getLooper().getQueue().postSyncBarrier();
 
     }
+
+    int token;
+
+    //往消息队列插入同步屏障
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void sendSyncBarrier(Handler handler) {
+        try {
+            Log.d("MainActivity", "插入同步屏障");
+            MessageQueue queue = handler.getLooper().getQueue();
+            Method method = MessageQueue.class.getDeclaredMethod("postSyncBarrier");
+            method.setAccessible(true);
+            token = (int) method.invoke(queue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //移除屏障
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void removeSyncBarrier(Handler handler) {
+        try {
+            Log.d("MainActivity", "移除屏障");
+            MessageQueue queue = handler.getLooper().getQueue();
+            Method method = MessageQueue.class.getDeclaredMethod("removeSyncBarrier", int.class);
+            method.setAccessible(true);
+            method.invoke(queue, token);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
